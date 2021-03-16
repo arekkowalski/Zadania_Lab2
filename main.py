55761f90bcbc2a539b680b78c7c3348e0e4f56cc
arekkowalski <arkkow8@wp.pl>
2021-03-16 14:25:49 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,76 @@
+import sys as system
+import math
+from math import *
+
+#Zadanie1
+# lista=['formuła1','koszykówka','siatkówka','UFC']
+# print(lista)
+#
+# lista.reverse()
+# print(lista)
+#
+# lista.append('piłka ręczna')
+# lista.append('piłka nożna')
+# print(lista)
+
+#Zadanie2
+# slownik ={}
+# slownik['al.']='aleja'
+# slownik['dr']='doktor'
+# slownik['cd.']='ciąg dalszy'
+#
+# print(slownik.values())
+
+#Zadanie3
+# gry={
+#     1: "The Witcher 3",
+#     2: "Valorant",
+#     3: "Forza Horizon 4",
+#     4: "Cyberpunk"
+# }
+# print("Liczba elementów")
+# print(len(gry))
+
+#Zadanie4
+# napis = input("Wpisz zdanie: ")
+# print(napis)
+# print(napis.count('a'))
+
+#Zadanie5
+# system.stdout.write("podaj a:")
+# a = int(system.stdin.readline())
+# system.stdout.write("podaj b:")
+# b = int(system.stdin.readline())
+# system.stdout.write("podaj c:")
+# c = int(system.stdin.readline())
+#
+# dzialanie = a**b+c
+# print((dzialanie))
+
+#Zadanie6
+
+# a = input("Podaj pierwszą liczbę: ")
+# b = input("Podaj drugą liczbę: ")
+# c = input("Podaj trzecią liczbę: ")
+#
+# a = int(a)
+# b = int(b)
+# c = int(c)
+#
+# if a > b and a > c:
+#     print("liczba " + str(a) + " jest większa")
+# elif b > a and b > c:
+#     print("liczba " + str(b) + " jest większa")
+# elif c > a and c > b:
+#     print("liczba " + str(c) + " jest większa")
+# else:
+#     print("wprowadzone liczby są równe")
+
+# #Zadanie7
+# liczby = [1,2,3.33,4.44]
+# i=0
+# for i in range(len(liczby)):
+#     liczby[i] = liczby[i] ** 2
+#     print(liczby[i])
+
+#Zadanie8
